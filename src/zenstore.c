1418823f56dbd321fa38ad18dcdf1777918da137
Michal Vyskocil <michal.vyskocil@gmail.com>
2016-06-01 21:26:17 +0200

    Problem: can't enter password from command line
    Solution: implement own getpass variant for unix

--- src/zenstore.c
+++ src/zenstore.c
@@ -18,6 +18,68 @@
 
 #include "zns_classes.h"
 
+#if defined __UNIX__
+#include <termios.h>
+
+// read up to crypto_secretbox_KEYBYTES from tty. Return allocated array or NULL
+// if the input is longer, warning is issues and key is stripped down
+static byte *
+s_getkey ()
+{
+    // source of "wisdom"
+    // http://www.gnu.org/software/libc/manual/html_node/getpass.html
+    // http://man7.org/tlpi/code/online/book/tty/no_echo.c.html
+
+    struct termios old, new_;
+    byte *ret = NULL;
+
+    /* Turn echoing off and fail if we can’t. */
+    int r = tcgetattr (STDOUT_FILENO, &old);
+    if (r != 0) {
+        zsys_error ("tcgetattr failed: %m");
+        return NULL;
+    }
+
+    new_ = old;
+    new_.c_lflag &= ~ECHO;
+    r = tcsetattr (STDIN_FILENO, TCSAFLUSH, &new_);
+    if (r != 0) {
+        zsys_error ("tcsetattr failed: %m");
+        return NULL;
+    }
+
+    ret = (byte*) zmalloc (crypto_secretbox_KEYBYTES);
+    if (!ret) {
+        zsys_error ("Allocating of buffer failed: %m");
+        goto end;
+    }
+
+    printf ("Enter the password: ");
+    fflush (stdout);
+
+    r = (int) read (STDIN_FILENO, ret, crypto_secretbox_KEYBYTES);
+    if (r == -1) {
+        zsys_error ("read failed: %m");
+        goto end;
+    }
+
+    for (int i = 0; i != crypto_secretbox_KEYBYTES; i++)
+        if (ret [i] == '\n') {
+            ret [i] = '\0';
+            break;
+        }
+
+end:
+    /* Restore terminal. */
+    (void) tcsetattr (STDIN_FILENO, TCSAFLUSH, &old);
+
+    return ret;
+}
+
+#else
+#error Please send a pull request with password reading for Windows
+#endif
+
 int main (int argc, char *argv [])
 {
     bool verbose = false;
@@ -42,5 +104,8 @@ int main (int argc, char *argv [])
     //  Insert main code here
     if (verbose)
         zsys_info ("zenstore - Daemon");
+
+    byte * key = s_getkey ();
+    zstr_free (&key);
     return 0;
 }
